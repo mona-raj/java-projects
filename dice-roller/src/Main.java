47ba647c6ceeef911c93d72bfb90f51c055888b9
Mona Raj <agrmonaraj@gmail.com>
2025-03-21 17:57:14 +0530

    Add ASCII art of dice

--- dice-roller/src/Main.java
+++ dice-roller/src/Main.java
@@ -20,7 +20,9 @@ public class Main {
         } else {
             for (int i = 0; i < noOfDice; i++) {
                 diceValue = random.nextInt(1, 7);
+                printDice(diceValue);
                 System.out.println("You rolled: " + diceValue);
+                System.out.println();
                 sum += diceValue;
                 Thread.sleep(1000);
             }
@@ -30,4 +32,52 @@ public class Main {
 
         scanner.close();
     }
+    static void printDice(int diceValue) {
+        String dice1 = """
+                ---------
+                |       |
+                |   ●   |
+                |       |
+                ---------""";
+        String dice2 = """
+                ---------
+                |     ● |
+                |       |
+                | ●     |
+                ---------""";
+        String dice3 = """
+                ---------
+                |     ● |
+                |   ●   |
+                | ●     |
+                ---------""";
+        String dice4 = """
+                ---------
+                | ●   ● |
+                |       |
+                | ●   ● |
+                ---------""";
+        String dice5 = """
+                ---------
+                | ●   ● |
+                |   ●   |
+                | ●   ● |
+                ---------""";
+        String dice6 = """
+                ---------
+                | ●   ● |
+                | ●   ● |
+                | ●   ● |
+                ---------""";
+
+        switch (diceValue) {
+            case 1 -> System.out.println(dice1);
+            case 2 -> System.out.println(dice2);
+            case 3 -> System.out.println(dice3);
+            case 4 -> System.out.println(dice4);
+            case 5 -> System.out.println(dice5);
+            case 6 -> System.out.println(dice6);
+        }
+    }
+
 }
